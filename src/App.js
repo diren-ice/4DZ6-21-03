364cc36ec0e88e89f942249e8707b09ac5e53dc4
aicholpon <120467115+diren-ice@users.noreply.github.com>
2023-03-14 00:22:19 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { fetchQueryPokemons } from "../src/components/API/fetchQuerryPokemon";
+import PokemonCard from "../src/components/PokemonCsrd/PokemonCard";
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState('dark');
+  const [pokemonList, setPokemonList] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const toogleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
+  const limit = 10
+
+  useEffect(() => {
+    fetchQueryPokemons(limit, offset)
+      .then((data) => {
+        setPokemonList(data?.results);
+      });
+  }, [offset])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${theme}`}>
+
+      <button
+        onClick={toogleTheme}
+        className="button">
+        Change Theme
+      </button>
+      <div className="container">
+        <div className="pokemonList">
+          {pokemonList?.map(pokemon => <PokemonCard pokemon={pokemon} />)}
+        </div>
+      </div>
     </div>
   );
 }
