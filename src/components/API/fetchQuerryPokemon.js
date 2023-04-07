eb5bf2e8d8ef0257219dba7687c97ca41fa5ea25
aicholpon <120467115+diren-ice@users.noreply.github.com>
2023-04-07 18:08:15 +0600

    Update fetchQuerryPokemon.js

--- src/components/API/fetchQuerryPokemon.js
+++ src/components/API/fetchQuerryPokemon.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import axios from "axios"
 const baseURL = 'https://pokeapi.co/api/v2/'
 
@@ -18,14 +18,4 @@ export const getPokemons = async(id) => {
     console.log(e);
  }
 }
-=======
-import axios from "axios"
-const baseURL = 'https://pokeapi.co/api/v2/'
 
-export const fetchQueryPokemons = async(limit, offset) => {
-    try {
-        const { data } = await axios.get(baseURL + `pokemon?limit=${limit}&offset=${offset}`)
-        return data;
-    }catch(e){}
-}
->>>>>>> 4608ad95c75e93278bd3f919d176dc552756c208
