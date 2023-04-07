dfacade771240c9c3dfb49b19d0246a0d671dfa8
aicholpon <120467115+diren-ice@users.noreply.github.com>
2023-04-07 18:11:03 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,48 +3,14 @@ import MainPage from './pages/MainPage/MainPage'
 import PokemonInfo from "./pages/PokemonInfo/PokemonInfo";
 import { Route, Routes } from "react-router-dom"
 
-
-import { useEffect, useState } from "react";
-import { fetchQueryPokemons } from "../src/components/API/fetchQuerryPokemon";
-import PokemonCard from "../src/components/PokemonCsrd/PokemonCard";
-
 const App = () => {
-  const [theme, setTheme] = useState('dark');
-  const [pokemonList, setPokemonList] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const toogleTheme = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark'
-    setTheme(newTheme)
-  }
-  const limit = 10
-
-  useEffect(() => {
-    fetchQueryPokemons(limit, offset)
-      .then((data) => {
-        setPokemonList(data?.results);
-      });
-  }, [offset])
-
+ 
   return (
     <div>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
           <Route path="/pokemon/:id" element={<PokemonInfo/>}  />
       </Routes>
-
-    <div className={`app ${theme}`}>
-
-      <button
-        onClick={toogleTheme}
-        className="button">
-        Change Theme
-      </button>
-      <div className="container">
-        <div className="pokemonList">
-          {pokemonList?.map(pokemon => <PokemonCard pokemon={pokemon} />)}
-        </div>
-      </div>
-    </div>
   )
 }
 
